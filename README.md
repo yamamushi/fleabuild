5a0d446099175d262473a1e3e1a412d631638153
yamamushi <yamamushi@gmail.com>
2015-08-12 23:38:39 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# fleabuild
+
+For when compiling is hilariously slow
+
+### Usage
+
+```bash
+$ ./fleabuild -f /path/to/file.mp3
+$ ./fleabuild -d /path/to/mp3/directory
+```
+
+### Development
+
+Pull requests welcomed!
+
---
+++ fleabuild.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+usage()
+{
+    echo "usage: fleabiuld options:<f|d|h>"
+}
+
+while getopts ":f:h:?:d:" opt; do
+  case $opt in
+    f)
+      audiotarget=$OPTARG
+      ;;
+    h|\?)
+      echo "Example Usage: fleabuild -f audiofile" >&2
+      ;;
+    d)
+	# Internal Field Separator set to newline, so file names with
+	# spaces do not break our script.
+IFS='
+'
+
+if [[ -d "$OPTARG" ]]
+then
+	star="/*.mp3"
+	files=($OPTARG$star)
+	audiotarget=${files[RANDOM % ${#files[@]}]}
+fi
+      ;;
+  esac
+done
+
+if [ -z $audiotarget ]
+then
+   echo "No arguments were given"
+   usage
+   exit
+fi
+
+afplay $audiotarget & fleapid=$! 
+trap 'kill $fleapid' EXIT HUP TERM INT
+
+time -p sh -c 'cmake .. && make'
+
+kill $fleapid 
